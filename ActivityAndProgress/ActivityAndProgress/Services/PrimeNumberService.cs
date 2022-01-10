519f090f1e632f1f01205477970b1f7944e0c63b
Martin Lenart <martin_lenart@icloud.com>
2022-01-10 20:25:26 +0100

    Add project files.

---
+++ ActivityAndProgress/ActivityAndProgress/Services/PrimeNumberService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ActivityAndProgress.Services
+{
+    public class PrimeNumberService
+    {   
+        public Task DisplayPrimeCountsAsync(int NrOfBatches, IProgress<(float, string)> onProgressReporting)
+        {
+            //Notice I can use async in Lambda Expression
+            return Task.Run(async () =>
+            {
+                for (int i = 0; i < NrOfBatches; i++)
+                {
+                    float k = i+1;
+                    int nrprimes = await GetPrimesCountAsync(i * 1000000 + 2, 1000000);
+                    onProgressReporting.Report((k/NrOfBatches, $"{nrprimes:N0} primes between {(i * 1000000):N0} and {((i + 1) * 1000000 - 1):N0}"));
+                }
+            });
+        }
+
+        public Task<int> GetPrimesCountAsync(int start, int count)
+        {
+            return Task.Run(() =>
+               ParallelEnumerable.Range(start, count).Count(n =>
+                 Enumerable.Range(2, (int)Math.Sqrt(n) - 1).All(i => n % i > 0)));
+        }
+
+    }
+}
